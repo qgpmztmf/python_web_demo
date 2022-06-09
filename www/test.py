cbe366771184ea9f2afca31d7061e9e7a4500220
qgpmztmf <qgpmztmf@gmail.com>
2022-06-09 15:49:41 +0800

    0609-1549

---
+++ www/test.py
@@ -0,0 +1,11 @@
+import asyncio
+import orm
+from models import User
+
+async def test(loop):
+    await orm.create_pool(user='ubuntu', password='mercedes12', db='awesome', loop=loop)
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='abut:blank')
+    await u.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
